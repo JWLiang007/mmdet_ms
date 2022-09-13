7f69c5b2ae84de7d7e2e509beecc9b60a1b45057
jwl <961055871@qq.com>
2022-09-14 00:30:54 +0800

    add config

--- workspace/configs/ssd/ssd512_proj.py
+++ workspace/configs/ssd/ssd512_proj.py
@@ -7,6 +7,31 @@ model = dict(
 dataset_type = 'CocoDataset'
 data_root = 'dataset/project/'
 CLASSES = ('person bev', 'car bev', 'van bev', 'truck bev','bus bev','person', 'car','aeroplane','bus', 'train' , 'truck',  'boat', 'bird',   'camouflage man')
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(512, 512), keep_ratio=False),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
 
 data = dict(
     samples_per_gpu=8,
@@ -19,7 +44,8 @@ data = dict(
             type=dataset_type,
             classes = CLASSES,
             img_prefix=data_root + 'images',
-            ann_file=data_root+'labels/train_all.json'
+            ann_file=data_root+'labels/train_all.json',
+            pipeline=train_pipeline
             )),
     val=dict(        
         type=dataset_type,
