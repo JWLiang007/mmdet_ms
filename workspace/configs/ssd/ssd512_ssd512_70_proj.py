59c799514d7018d2365751ed8dbe14a0a3c24263
jwl <961055871@qq.com>
2022-09-14 21:05:12 +0800

    add ssd psd config

---
+++ workspace/configs/ssd/ssd512_ssd512_70_proj.py
@@ -0,0 +1,14 @@
+_base_ = 'ssd512_proj.py'
+
+data_root = 'dataset/project/'
+data = dict(
+    train=dict(
+        dataset=dict(
+            ann_file=data_root+'labels/psd_ssd512_train_70.json',
+            )),
+    val=dict(        
+
+        ann_file=data_root+'labels/psd_ssd512_test_70.json'),
+    test=dict(        
+
+        ann_file=data_root+'labels/psd_ssd512_test_70.json'))
